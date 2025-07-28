3cb06c036d1e7b03a359a77a66fe92b1c8630ec4
jimmy914 <jimmy7620@naver.com>
2025-07-28 21:41:43 +0900

    s2:lecture8_bean설정

--- src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
@@ -1,11 +1,9 @@
 package com.in28minutes.learn_spring_framework;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
-import com.in28minutes.learn_spring_framework.game.MarioGame;
 import com.in28minutes.learn_spring_framework.game.PackmanGame;
-import com.in28minutes.learn_spring_framework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
     public static void main(String[] args) {
 
