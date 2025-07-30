a57fec692ce4592bb3010b598070294bd3c1bb65
siyeon <jimmy7620@naver.com>
2025-07-30 14:28:22 +0900

    S2:lecture10_bean 주입하는 다양한 방법, 오류존재하는 코드임

--- src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 //record를 사용하면 게터 세터 메소드 설정해줄 필요 없음
-record Person (String name, int age) {
+record Person (String name, int age, Address address) {
 
 };
 
@@ -27,11 +27,26 @@ public class HelloWorldConfiguration {
 
     @Bean
     public Person person() {
-        return new Person("Ranga",15);
+        return new Person("Ravi",20,new Address("광안리","busan"));
     }
 
     @Bean
+    public Person person2MethodCall() {
+        return new Person(name(),age(),address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) {
+        return new Person(name,age,address3);
+    }
+
+    @Bean(name = "address2")
     public Address address() {
         return new Address("gurogu","seoul");
+    }
+
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("천안","충청도");
     }
 }
